f750a72cdb76234879a49e18dae612eec00a9ce9
Maxime St-Pierre <me@maximest-pierre.me>
2018-02-04 08:15:13 -0500

    Add composer install to dev.sh

--- dev.sh
+++ dev.sh
@@ -7,4 +7,8 @@ if [ ! -e "data/config.php" ]; then
   cp "$DIR/data/config.php.example" "$DIR/data/config.php"
 fi
 
+# Install the dependecies
+php composer.phar install
+
+
 php7.0 --server localhost:8000 --docroot "$DIR/web" "$DIR/web/index.php"
